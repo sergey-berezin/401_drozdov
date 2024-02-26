fef3b28a90b96703a0e20ceaddc1f3da6ba64fb2
Nikita Drozdov <drozdovna@my.msu.ru>
2024-02-26 14:33:11 +0300

    added task 2a

---
+++ LAB2/LAB2/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using LAB2.Model;
+using System.Windows;
+
+
+namespace LAB2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new AppViewModel();
+        }
+
+
+        
+    }
+}
